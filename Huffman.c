56a62dc6b67eef0b887215cc226f755184ce7b08
Andrieux <andrieuxliam@gmail.com>
2020-03-07 10:25:26 +0100

    Huffman

--- Huffman.c
+++ Huffman.c
@@ -10,6 +10,14 @@ struct code_char {
 };
 
 
+int power(int a,int n){
+    if(n==1)return a;
+    if(n==0)return 1;
+    if(n%2==0)return power(a*a,n/2);
+    else return a*power(a*a,(n-1)/2);
+}
+
+
 
 
 /*
@@ -111,12 +119,79 @@ void AfficherCode(int nb,struct code_char HuffmanCode[]){
     }
 }
 
+void AfficherCode_double(int nb,struct code_char HuffmanCode[]){
+    for(int i=0;i<nb;i++){
+        for(int j=0;j<nb;j++){
+            printf("Evenement %d,%d: ",i+1,j+1);
+            for(int h=0;h<HuffmanCode[1+i*nb+j].lg;h++){
+                printf("%d",HuffmanCode[1+i*nb+j].code[h]);
+            }
+            printf("\n");
+        }
+    }
+}
+
+
+
+
+void AfficherCode_n(int nb,int n,struct code_char HuffmanCode[]){
+    for(int i=0;i<(power(nb,n));i++){
+        printf("Evenement");
+        for(int j=n-1;0<=j;j--){  
+            int index=((i)/power(nb,j))%nb;  
+            printf(" %d ",index+1);        
+        }
+        for(int h=0;h<HuffmanCode[1+i].lg;h++){
+                printf("%d",HuffmanCode[1+i].code[h]);
+            }
+        printf("\n"); 
+    }
+}
+
+
+float Longueur_moy(int nb,struct code_char HuffmanCode[],float* tab,int m){
+    float res=0;
+    for(int i=1;i<nb+1;i++){
+        res+=HuffmanCode[i].lg*tab[i-1];
+    }
+    printf("Longueur moyenne du code : %f \n",res/m);
+    return res;
+}
+
+/*
+float* tab_mul(float* t,float* t1,int m,int nb,int nb2){
+    if(m<=0)return NULL;
+    if(m==1)return t;
+    float* t2=malloc(sizeof(float)*nb*nb2);
+    for(int i=0;i<nb;i++){
+        for(int j=0;j<nb2;j++){
+            t2[i*nb+j]=t[i]*t1[j];
+        }
+    }
+    float* res=tab_mul(t,t2,m-1,nb,nb*nb2);
+    free(t2);
+    return res;
+}
+*/
+float* tab_mul(float* t,int m,int nb){
+    int pow=power(nb,m);
+    float* t3=malloc(sizeof(float)*pow);
+    for(int i=0;i<pow;i++){
+        t3[i]=1;
+        for(int j=0;j<m;j++){
+            int index=((i)/power(nb,j))%nb;
+            t3[i]*=t[index];
+        }
+    }
+    return t3;
+}
+
 
 
 int main(int argc, char** argv){   
     FILE *src;
-    if(argc<2){
-        printf("Syntaxe : %s [fichier contenant les proba]\n",argv[0]);
+    if(argc<3){
+        printf("Syntaxe : %s [fichier contenant les proba] [multiplicité des événements]\n",argv[0]);
         return EXIT_FAILURE;
     }
     src = fopen(argv[1],"r");
@@ -124,6 +199,7 @@ int main(int argc, char** argv){
         printf("Erreur ouverture du fichier\n");
         return EXIT_FAILURE;
     }
+    int m=atoi(argv[2]);
     int nb;
     int res;
     int i =0;
@@ -145,7 +221,8 @@ int main(int argc, char** argv){
         printf("%d au lieu de %d \n",i,nb);
         return EXIT_FAILURE;
     }
-    struct code_char HuffmanCode[50];
+    int nb3=power(nb,m);
+    struct code_char HuffmanCode[nb3];
     fap f;
     Arbre a;
     f= InitHuffman(tab,nb);
@@ -153,6 +230,34 @@ int main(int argc, char** argv){
     ConstruireCode(a,tab,HuffmanCode);
     AfficherCode(nb,HuffmanCode);
     AfficherArbre(a);
+    Longueur_moy(nb,HuffmanCode,tab,1);
+
+    float tab2[nb*nb];
+
+    for(int i=0;i<nb;i++){
+        for(int j=0;j<nb;j++){
+            tab2[i*nb+j]=tab[i]*tab[j];
+        }
+    }
+    f= InitHuffman(tab2,nb*nb);
+    a=ConstruireArbre(f);
+    ConstruireCode(a,tab2,HuffmanCode);
+    AfficherCode_double(nb,HuffmanCode);
+    Longueur_moy(nb*nb,HuffmanCode,tab2,2);
+
+    float* tab3=tab_mul(tab,m,nb);
+    //float* tab3=tab_mul(tab,tab,m,nb,nb);
+    float res5=0;
+    for(int i=0;i<power(nb,m);i++)res5+=tab3[i];
+    if(res5<0.95  || res5 >1.05){printf("ERRRE U R   %f",res5); exit(-1);   }
+    printf("%f \n\n\n",res5);
+    
+    f= InitHuffman(tab3,nb3);
+    a=ConstruireArbre(f);
+    ConstruireCode(a,tab3,HuffmanCode);
+    AfficherCode_n(nb,m,HuffmanCode);
+    Longueur_moy(nb3,HuffmanCode,tab3,m);
+    
 
     return EXIT_SUCCESS;
 }
